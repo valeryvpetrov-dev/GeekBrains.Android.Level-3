9fe5ccef7dc01af3198a96ab3f6c41e44e876dd1
Valery <valera071998@gmail.com>
2019-09-18 15:13:30 +0600

    add rxjava implementation of text change listener

--- app/src/main/java/ru/geekbrains/android/level3/valeryvpetrov/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/android/level3/valeryvpetrov/MainActivity.kt
@@ -1,35 +1,76 @@
 package ru.geekbrains.android.level3.valeryvpetrov
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var compositeDisposable: CompositeDisposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        compositeDisposable = CompositeDisposable()
     }
 
     override fun onStart() {
         super.onStart()
-        input.addTextChangedListener(object : TextWatcher {
-            
-            override fun afterTextChanged(p0: Editable?) {
-                p0?.let {
-                    inputMirror.text = it.toString()
+        compositeDisposable.add(
+            Observable.create(ObservableOnSubscribe<String> {
+                val inputTextWatcher = object : TextWatcher {
+                    override fun afterTextChanged(s: Editable?) {
+                        if (!it.isDisposed) {
+                            it.onNext(s.toString())
+                        }
+                    }
+
+                    override fun beforeTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        count: Int,
+                        after: Int
+                    ) {
+
+                    }
+
+                    override fun onTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        before: Int,
+                        count: Int
+                    ) {
+
+                    }
+
                 }
-            }
+                it.setCancellable {
+                    input.removeTextChangedListener(inputTextWatcher)
+                }
+                input.addTextChangedListener(inputTextWatcher)
+            })
+                .debounce(200, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { text ->
+                    inputMirror.text = text
+                }
+        )
+    }
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    override fun onStop() {
+        super.onStop()
+        compositeDisposable.clear()
+    }
 
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-        })
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
