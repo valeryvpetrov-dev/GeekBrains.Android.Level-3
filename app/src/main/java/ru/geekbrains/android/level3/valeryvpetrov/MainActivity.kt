c70464c019692077f7b51aa3f5e136247e96f02e
Valery <valera071998@gmail.com>
2019-09-18 14:24:11 +0600

    add non-reactive implementation of text change listener

--- app/src/main/java/ru/geekbrains/android/level3/valeryvpetrov/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/android/level3/valeryvpetrov/MainActivity.kt
@@ -2,11 +2,34 @@ package ru.geekbrains.android.level3.valeryvpetrov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        input.addTextChangedListener(object : TextWatcher {
+            
+            override fun afterTextChanged(p0: Editable?) {
+                p0?.let {
+                    inputMirror.text = it.toString()
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        })
     }
 }
