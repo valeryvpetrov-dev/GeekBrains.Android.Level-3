a39e658eb5ee6c8b15ac3f5d067407a9a7236fe1
Valery <valera071998@gmail.com>
2019-09-18 15:40:51 +0600

    add rxbinding implementation of text change listener

--- app/src/main/java/ru/geekbrains/android/level3/valeryvpetrov/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/android/level3/valeryvpetrov/MainActivity.kt
@@ -1,11 +1,8 @@
 package ru.geekbrains.android.level3.valeryvpetrov
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.Observable
-import io.reactivex.ObservableOnSubscribe
+import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,42 +21,12 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         compositeDisposable.add(
-            Observable.create(ObservableOnSubscribe<String> {
-                val inputTextWatcher = object : TextWatcher {
-                    override fun afterTextChanged(s: Editable?) {
-                        if (!it.isDisposed) {
-                            it.onNext(s.toString())
-                        }
-                    }
-
-                    override fun beforeTextChanged(
-                        s: CharSequence?,
-                        start: Int,
-                        count: Int,
-                        after: Int
-                    ) {
-
-                    }
-
-                    override fun onTextChanged(
-                        s: CharSequence?,
-                        start: Int,
-                        before: Int,
-                        count: Int
-                    ) {
-
-                    }
-
-                }
-                it.setCancellable {
-                    input.removeTextChangedListener(inputTextWatcher)
-                }
-                input.addTextChangedListener(inputTextWatcher)
-            })
+            RxTextView.textChangeEvents(input)
+                .skipInitialValue()
                 .debounce(200, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { text ->
-                    inputMirror.text = text
+                .subscribe { event ->
+                    inputMirror.text = event.text().toString()
                 }
         )
     }
